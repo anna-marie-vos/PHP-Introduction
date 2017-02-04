b052dd91a7dda65338eb9a1acde6bf623c07efa9
anna-marie-vos <amvos@outlook.com>
2017-02-05 11:17:10 +1300

    tut 7 form using post

---
+++ tut7-postMethod.php
@@ -0,0 +1,10 @@
+<?php
+  $number = $_POST['number'];
+  $string = $_POST['string'];
+
+  if($number === '16' and $string === 'hello'){
+    echo "you guessed it correctly, the number is 16 and the string is hello";
+  } else{
+    echo "try again";
+  }
+?>
