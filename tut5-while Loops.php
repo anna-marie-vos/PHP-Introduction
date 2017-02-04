4cf69ad3f383c2349124d575bd5d035060349ff6
anna-marie-vos <amvos@outlook.com>
2017-02-05 10:54:53 +1300

    tut6 form works

--- tut5-while Loops.php
+++ tut5-while Loops.php
@@ -1,3 +1,13 @@
 <?php
+$x =0;
+while($x<10){
+  echo "While loop: the number is $x </br>";
+  $x++;
+}
 
+$names = array('chris', 'tom', 'bob');
+
+foreach($names as $person){
+  echo "This person is $person </br>";
+}
 ?>
---
+++ tut6-dynamicVars.php
@@ -0,0 +1,7 @@
+<?php
+//get a value from the browser
+//type this in the browser: http://localhost/PHP-Introduction/tut6-dynamicVars.php?name=Tom
+  $name = $_GET['name'];
+  echo "Hello $name";
+
+?>
